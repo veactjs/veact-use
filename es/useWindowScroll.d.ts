ec973584f6ab34c7333c1b275d5b2b72b7873430
surmon-china <surmon@foxmail.com>
2021-07-27 04:28:29 +0800

    chore: add em dir to gitignore

--- es/useWindowScroll.d.ts
+++
@@ -1,5 +0,0 @@
-export interface WindowScrollHookState {
-    x: number;
-    y: number;
-}
-export declare const useWindowScroll: () => WindowScrollHookState;
--- es/useWindowSize.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const useWindowSize: (initialWidth?: number, initialHeight?: number) => {
-    width: number;
-    height: number;
-};
