b149015f6cc4011e61c11e3e03366eee8f333e4e
Surmon <surmon.me@gmail.com>
2024-08-15 06:22:42 +0800

    chore: NIP

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
-export default {
+import type { Config } from 'jest'
+
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   coverageDirectory: 'coverage',
 }
+
+export default config
